bb9e7b32dcb4da417652fc3d4c7b3ce356ea4fb4
murali <murali@murali-PC.attlocal.net>
2021-10-20 22:01:19 -0700

    Second comit

--- src/Training1/Hello1.java
+++ src/Training1/Hello1.java
@@ -3,7 +3,8 @@ package Training1;
 public class Hello1 {
 
 	public static void main(String[] args) {
-	System.out.println("welcome to usa");	
+	System.out.println("welcome to usa");
+	System.out.println("welcome to London");
 
 	}
 
