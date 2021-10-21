e1d145a4ef261e2b627e64921d93e4287f75cddd
murali <murali@murali-PC.attlocal.net>
2021-10-20 22:07:24 -0700

    welcome to tracy comitted

--- src/Training1/Hello1.java
+++ src/Training1/Hello1.java
@@ -5,6 +5,7 @@ public class Hello1 {
 	public static void main(String[] args) {
 	System.out.println("welcome to usa");
 	System.out.println("welcome to London");
+	System.out.println("wecome to Tracy");
 
 	}
 
