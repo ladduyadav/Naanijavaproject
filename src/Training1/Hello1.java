fe424f57d121178e6d7479fefac0b92848fa87bd
murali <murali@murali-PC.attlocal.net>
2021-10-23 21:35:09 -0700

    chicago updated

--- src/Training1/Hello1.java
+++ src/Training1/Hello1.java
@@ -6,7 +6,7 @@ public class Hello1 {
 	System.out.println("welcome to usa");
 	System.out.println("welcome to London");
 	System.out.println("wecome to Tracy");
-	System.out.println("wecome to chicago");
+	System.out.println("wecome to chicago usa");
 
 	}
 
